0084adfaaf197de340ac81f5e74c4e01074d4876
Maria Trujillo <mptruj015@gmail.com>
2020-09-14 23:19:14 +0200

    finished binary search, reached mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -16,6 +16,22 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    low = 0
+    high = len(arr) - 1
+    
+
+    while low <= high:
+        middle = (low + high)
+        guess = arr[middle]
+        
+        if guess == target:
+            return middle
+        elif guess > target:
+            high = middle - 1
+        else:
+            low = middle - 1
+    return -1
+
 
 
     return -1  # not found
