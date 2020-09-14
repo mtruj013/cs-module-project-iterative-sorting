91cd5614edd1b0ff64bc9fc2c7727b642da7ee02
Maria Trujillo <mptruj015@gmail.com>
2020-09-14 23:13:05 +0200

    finished linear search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,8 +1,15 @@
 def linear_search(arr, target):
     # Your code here
-
-
-    return -1   # not found
+    if not arr:
+        return -1
+    else: 
+        for i in range(len(arr)):
+            if arr[i] == target:
+                return i
+            elif i == len(arr) - 1:
+                return -1
+            i += 1
+    
 
 
 # Write an iterative implementation of Binary Search
